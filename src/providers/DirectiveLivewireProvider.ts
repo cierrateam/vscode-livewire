50e9495d7f05afb45a98384181d841c2a4426539
Milorad.P <miloradpopovic451@gmail.com>
2020-06-02 18:42:22 +0200

    update directive livewire provider and get params utility function

--- src/providers/DirectiveLivewireProvider.ts
+++ src/providers/DirectiveLivewireProvider.ts
@@ -25,14 +25,140 @@ export class DirectiveLivewireColonProvider {
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
         let linePrefix = document.lineAt(position).text.substr(0, position.character);
-        if (!linePrefix.endsWith(' wire:')) {
-            return undefined;
+
+        let attributes: string[] = [];
+        if (linePrefix.endsWith(' wire:')) {
+            attributes = [
+                'key',
+                'model',
+                'poll',
+                'init',
+                'loading',
+                'dirty',
+                'target',
+                'ignore',
+                'abort',
+                'afterprint',
+                'animationend',
+                'animationiteration',
+                'animationstart',
+                'beforeprint',
+                'beforeunload',
+                'blur',
+                'canplay',
+                'canplaythrough',
+                'change',
+                'click',
+                'contextmenu',
+                'copy',
+                'cut',
+                'dblclick',
+                'drag',
+                'dragend',
+                'dragenter',
+                'dragleave',
+                'dragover',
+                'dragstart',
+                'drop',
+                'durationchange',
+                'ended',
+                'error',
+                'focus',
+                'focusin',
+                'focusout',
+                'fullscreenchange',
+                'fullscreenerror',
+                'hashchange',
+                'input',
+                'invalid',
+                'keydown',
+                'keypress',
+                'keyup',
+                'load',
+                'loadeddata',
+                'loadedmetadata',
+                'loadstart',
+                'message',
+                'mousedown',
+                'mouseenter',
+                'mouseleave',
+                'mousemove',
+                'mouseover',
+                'mouseout',
+                'mouseup',
+                'offline',
+                'online',
+                'open',
+                'pagehide',
+                'pageshow',
+                'paste',
+                'pause',
+                'play',
+                'playing',
+                'popstate',
+                'progress',
+                'ratechange',
+                'resize',
+                'reset',
+                'scroll',
+                'search',
+                'seeked',
+                'seeking',
+                'select',
+                'show',
+                'stalled',
+                'storage',
+                'submit',
+                'suspend',
+                'timeupdate',
+                'toggle',
+                'touchcancel',
+                'touchend',
+                'touchmove',
+                'touchstart',
+                'transitionend',
+                'unload',
+                'volumechange',
+                'waiting',
+                'wheel',
+            ];
+        } else if (linePrefix.endsWith('wire:click.')) {
+            attributes = [
+                'prefetch',
+            ];
+        } else if (linePrefix.endsWith('wire:keydown.')) {
+            attributes = [
+                'enter',
+            ];
+        } else if (linePrefix.endsWith('wire:model.')) {
+            attributes = [
+                'debounce',
+                'lazy',
+            ];
+        } else if (linePrefix.endsWith('wire:loading.') || linePrefix.endsWith('wire:dirty.') || linePrefix.endsWith('wire:offline.')) {
+            attributes = [
+                'class',
+                'attr',
+            ];
+        } else if (linePrefix.endsWith('wire:loading.class.') || linePrefix.endsWith('wire:dirty.class.') || linePrefix.endsWith('wire:offline.class.')) {
+            attributes = [
+                'remove',
+            ];
+        } else if (linePrefix.endsWith('wire:ignore.')) {
+            attributes = [
+                'self',
+            ];
         }
 
-        return [
-            new vscode.CompletionItem('morning', vscode.CompletionItemKind.Keyword),
-            new vscode.CompletionItem('afternoon', vscode.CompletionItemKind.Keyword),
-            new vscode.CompletionItem('evening', vscode.CompletionItemKind.Keyword),
-        ];
+        const completions = attributes.map(itm => {
+            const completion = new vscode.CompletionItem(itm, vscode.CompletionItemKind.Field);
+            completion.commitCharacters = ['.'];
+
+            return completion;
+        });
+
+        return completions;
+
+
     }
 }
--- src/util/util.ts
+++ src/util/util.ts
@@ -9,10 +9,86 @@ export const getAllComponents = async () => {
         for (let i = 0; i < doc.lineCount; i++) {
             const match = /'(.*)'\s*=>\s*'(.*)',*/g.exec(doc.lineAt(i).text);
             if (match && match.length) {
-                completions.push(match[1]);
+                const params = await getComponentParams(match[1], match[2]);
+                completions.push({
+                    name: match[1],
+                    params,
+                });
             }
         }
     } catch (e) {}
 
+
     return completions;
+};
+
+export const getComponentParams = async (component: string, srcFile?: string) => {
+    let params = [];
+
+    try {
+        if (!srcFile) {
+            const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(`${vscode.workspace.rootPath}/bootstrap/cache/livewire-components.php`));
+            const match = new RegExp(`'${component}'\\s*=>\\s*'(.*)',*`, 'mg').exec(doc.getText());
+            if (match && match.length) {
+                srcFile = match[1];
+            }
+        }
+        
+        if (!srcFile) {
+            return null;
+        }
+        
+        component = covertKebabCaseToPaskalCase(component);
+        const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(`${vscode.workspace.rootPath}/${srcFile}.php`));
+        let match = new RegExp(`class\\s*${component}[^{]*{[^]*`, 'mg').exec(doc.getText());
+        if (!match) {
+            return null;
+        }
+        const regex = /[{|}]/mg;
+        let braceNum1 = 0, braceNum2 = 0;
+        do {
+            const braceMatch = regex.exec(match[0]);
+            if (!braceMatch) {
+                break;
+            }
+            if (braceMatch[0] === '{') {
+                braceNum1 ++;
+            }
+            if (braceMatch[0] === '}') {
+                braceNum2 ++;
+            }
+            if (braceNum1 === braceNum2) {
+                break;
+            }
+        } while(true);
+        
+        if (!regex.lastIndex) {
+            return null;
+        }
+        
+        const classText = match[0].substr(0, regex.lastIndex);
+        match = /function\s*mount\s*\(([^\)]*)/mg.exec(classText);
+        if (!match || !match.length) {
+            return [];
+        }
+        const variables = match[1].split(/,/mg);
+        for (const variable of variables) {
+            const words = variable.split(/\$/mg);
+            if (words.length) {
+                params.push(words[1]);
+            }
+        }
+    } catch (e) {}
+
+    return params;
+};
+
+export const covertKebabCaseToPaskalCase = (text: string) => {
+    let resText = '';
+    const words = text.split('-');
+    for (const word of words) {
+        resText += word[0].toUpperCase();
+        resText += word.substr(1);
+    }
+    return resText;
 };
